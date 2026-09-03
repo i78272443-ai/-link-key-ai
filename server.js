0ae57c6b6aeed6cccdc8126c0eff8da2e5b557b7
i78272443-ai <i78272443@gmail.com>
2026-09-03 18:02:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json({ limit: "1mb" }));
-app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
 function validUrl(value) {
   try {
